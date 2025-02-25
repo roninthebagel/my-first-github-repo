040f574498d8ae560c937d6577035b1cb6f1d53b
roninthebagel <ronin@run2online.com>
2025-02-25 12:35:48 +0000

    removed credentials

--- script.R
+++ script.R
@@ -1,15 +1,5 @@
 # Required packages
 install.packages("usethis")
 install.packages("gitcreds")
-
-#connecting credentials to github
-library("usethis") 
-use_git_config(user.name  = "roninthebagel",
-               user.email = "ronin@run2online.com")
-
-usethis::create_github_token()
-gitcreds::gitcreds_set()
-usethis::gh_token_help()
-
 install.packages("tidyverse")
 install.packages("palmerpenguins")
